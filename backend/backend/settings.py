e61c25c6e44ca709d50bfd606d87e520643fbde0
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-25 16:47:27 +0500

    Корректировка структуры проекта

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
+    "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
