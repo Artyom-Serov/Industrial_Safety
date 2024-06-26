a18e9bad7f70cdfb2f52cc31761b80080b9ab392
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-26 16:25:29 +0500

    Исправления формы пользователя для редактирования

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
@@ -17,7 +17,15 @@ class CustomUserCreationForm(UserCreationForm):
         )
 
 
-class CustomUserEditForm(UserChangeForm):
+class CustomUserEditForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='При необходимости смены пароля, укажите его',
+        required=False, widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Введите новый пароль повторно', required=False, widget=forms.PasswordInput
+    )
+
     class Meta:
         model = User
         fields = (
@@ -25,6 +33,24 @@ class CustomUserEditForm(UserChangeForm):
             'organization',
             'email',
             'first_name',
-            'last_name',
-            'password'
+            'last_name'
         )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password1 != password2:
+            self.add_error('password2', "Пароли должны совпадать.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
