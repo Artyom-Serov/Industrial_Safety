275c62057ff5e8c4eca86fc439d51798ef01edd0
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-26 16:20:56 +0500

    Настройка отображения шаблонов

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.parent / 'frontend' / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
