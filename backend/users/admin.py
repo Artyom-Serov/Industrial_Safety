7991ff1cbcdef9fdab059f94cd0146f72d2433da
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-26 16:24:23 +0500

    Исправления админ-панели для добавления пользователей

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .forms import CustomUserEditForm, CustomUserCreationForm
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserEditForm
+    model = User
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'organization',
+        'is_staff',
+        'is_active'
+    )
+    list_filter = ('is_staff', 'is_active', 'organization')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': (
+            'first_name', 'last_name', 'email', 'organization'
+        )}),
+        ('Permissions', {'fields': (
+            'is_staff', 'is_active', 'groups', 'user_permissions'
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'first_name', 'last_name',
+                'organization', 'password1', 'password2', 'is_staff',
+                'is_active'
+            )}
+        ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+admin.site.register(User, CustomUserAdmin)
