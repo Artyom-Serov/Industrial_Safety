d023618a5d6341edd02b1df197d6fa190bd79506
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-06-27 11:29:44 +0500

    Настройка структуры для основного приложения с базой

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users.apps.UsersConfig",
-    "base.apps.BaseConfig",
+    "facility.apps.FacilityConfig",
 ]
 
 MIDDLEWARE = [
