72ac437fb5a268a5711421d32a04c134dbae8cee
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-10-07 09:06:48 +0500

    Добавление генератора документов

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users.apps.UsersConfig",
     "facility.apps.FacilityConfig",
+    "documents.apps.DocumentsConfig",
 ]
 
 MIDDLEWARE = [
