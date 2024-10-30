47a23bfbb69ff492b39895b05b44c70871e6c077
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-10-30 09:04:17 +0500

    Добавлены URL's для генерации документа

---
+++ backend/documents/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import document_generate_view
+
+app_name = 'documents'
+
+urlpatterns = [
+    path('generate/<int:examination_id>/', document_generate_view, name='document_generate')
+]
