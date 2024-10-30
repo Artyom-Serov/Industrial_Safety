c056a165b6b4764cc203239624c67645ff5a6b58
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-10-30 21:45:12 +0500

    Исключёна конвертация в pdf

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -3,7 +3,6 @@ import os
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
-from docx2pdf import convert
 from .forms import DocumentGenerationForm
 from .document_generation import generate_document
 from facility.models import Examination
@@ -17,28 +16,32 @@ def document_generate_view(request, examination_id):
     if request.method == 'POST':
         form = DocumentGenerationForm(request.POST)
         if form.is_valid():
-            # получаем выбранный шаблон и формат файла
+            # получаем выбранный шаблон
             template = form.cleaned_data['template']
-            file_format = form.cleaned_data['file_format']
 
             # путь к шаблону и временное имя файла для сохранения
-            template_path = os.path.join(settings.BASE_DIR, 'documents', 'templates', f"{template}.docx")
-            output_name = f"{template}_{examination_id}.{file_format}"
-            output_path = os.path.join(settings.BASE_DIR, 'generated_documents', output_name)
+            template_path = os.path.join(
+                settings.BASE_DIR, 'documents', 'templates',
+                f"{template}.docx"
+            )
+            output_name = f"{template}_{examination_id}.docx"
+            output_path = os.path.join(
+                settings.BASE_DIR, 'generated_documents', output_name
+            )
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
             # генерация документа в формате .docx
             generate_document(examination_id, template_path, output_path)
 
-            # если выбран формат PDF, конвертируем .docx в .pdf
-            if file_format == 'pdf':
-                convert(output_path)
-                output_path = output_path.replace('.docx', '.pdf')
-
             # отправляем сгенерированный файл в ответ
             with open(output_path, 'rb') as file:
-                response = HttpResponse(file.read(), content_type='application/pdf' if file_format == 'pdf' else 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-                response['Content-Disposition'] = f'attachment; filename="{output_name}"'
+                response = HttpResponse(
+                    file.read(),
+                    content_type='application/vnd.openxmlformats-'
+                                 'officedocument.wordprocessingml.document'
+                )
+                response['Content-Disposition'] = (f'attachment; filename="'
+                                                   f'{output_name}"')
                 return response
 
     else:
