2cbc68d117c4f15630fed9387a8983fc2821c43a
Artyom-Serov <artyomserov@yandex.ru>
2024-11-12 00:12:48 +0500

    Подключение кэширования генерации документов в documents.views

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -1,6 +1,10 @@
+"""
+Модуль представлений для управления генерации документов.
+"""
 import os
 
 from django.conf import settings
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import DocumentGenerationForm
@@ -24,33 +28,49 @@ def document_generate_view(request, examination_id):
     Возвращает:
     - HttpResponse с прикрепленным документом в формате .docx для загрузки.
     """
-    examination = Examination.objects.get(id=examination_id)
+    cache_key_examination = f'examination_{examination_id}'
+    examination = cache.get(cache_key_examination)
+    if not examination:
+        examination = Examination.objects.get(id=examination_id)
+        cache.set(cache_key_examination,
+                  examination,
+                  timeout=settings.CACHE_TTL)
 
     if request.method == 'POST':
         form = DocumentGenerationForm(request.POST)
         if form.is_valid():
             template = form.cleaned_data['template']
-            template_path = os.path.join(
-                settings.BASE_DIR, 'documents', 'templates',
-                f"{template}.docx"
-            )
             output_name = f"{template}_{examination_id}.docx"
             output_path = os.path.join(
                 settings.BASE_DIR, 'generated_documents', output_name
             )
-            os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-            generate_document(examination_id, template_path, output_path)
-
-            with open(output_path, 'rb') as file:
-                response = HttpResponse(
-                    file.read(),
-                    content_type='application/vnd.openxmlformats-'
-                                 'officedocument.wordprocessingml.document'
+            cache_key_document = f'document_{output_name}'
+            cached_document = cache.get(cache_key_document)
+            if not cached_document:
+                template_path = os.path.join(
+                    settings.BASE_DIR, 'documents', 'templates',
+                    f"{template}.docx"
                 )
-                response['Content-Disposition'] = (f'attachment; filename="'
-                                                   f'{output_name}"')
-                return response
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
+                generate_document(examination_id, template_path, output_path)
+
+                with open(output_path, 'rb') as file:
+                    document_content = file.read()
+                    cache.set(cache_key_document,
+                              document_content,
+                              timeout=settings.CACHE_TTL)
+            else:
+                document_content = cached_document
+
+            response = HttpResponse(
+                document_content,
+                content_type='application/vnd.openxmlformats-'
+                                 'officedocument.wordprocessingml.document'
+            )
+            response['Content-Disposition'] = (f'attachment; filename="'
+                                               f'{output_name}"')
+            return response
 
     else:
         form = DocumentGenerationForm()
