e7d8fee3f7fc1ba46beb01d19ccba599845c244f
Artyom-Serov <artyomserov@yandex.ru>
2024-11-13 18:06:04 +0500

    Настройка запуска pytest

--- backend/manage.py
+++ backend/manage.py
@@ -14,6 +14,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if 'test' in sys.argv:
+        import pytest
+        sys.exit(pytest.main())
     execute_from_command_line(sys.argv)
 
 
